32e9a57e43fc3fad9cbf48f1ac36001d56fec28a
Fatih <fdonmez_1512@hotmail.com>
2025-07-30 16:07:07 +0300

    refactor: clean up images controller and service by removing unused methods

--- src/images/images.controller.ts
+++ src/images/images.controller.ts
@@ -1,7 +1,6 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { ImagesService } from './images.service';
 import { CreateImageDto } from './dto/create-image.dto';
-import { UpdateImageDto } from './dto/update-image.dto';
 
 @Controller('images')
 export class ImagesController {
@@ -10,25 +9,5 @@ export class ImagesController {
   @Post()
   create(@Body() createImageDto: CreateImageDto) {
     return this.imagesService.create(createImageDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.imagesService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.imagesService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateImageDto: UpdateImageDto) {
-    return this.imagesService.update(+id, updateImageDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.imagesService.remove(+id);
   }
 }
--- src/images/images.service.ts
+++ src/images/images.service.ts
@@ -7,20 +7,4 @@ export class ImagesService {
   create(createImageDto: CreateImageDto) {
     return 'This action adds a new image';
   }
-
-  findAll() {
-    return `This action returns all images`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} image`;
-  }
-
-  update(id: number, updateImageDto: UpdateImageDto) {
-    return `This action updates a #${id} image`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} image`;
-  }
 }
